d2ffeed3b7877b63ff1347707b3745c1b61ddbe7
RFBS18 <fabriziobarrios22@gmail.com>
2023-04-29 10:24:52 -0500

    configuracion global y de DataTables

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    define("HOST", "localhost");
+    define("USER", "id20611277_clinica");
+    define("PASS", "9L!s8+S(US_E]u(J");
+    define("DATABASE", "id20611277_sistemafarmacia");
+    define("PORT", "3306");
+    define("CHARSET", "utf8");
+    */
+
+    define("DRIVER", "mysql");
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DATABASE", "sistemafarmacia");
+    define("PORT", "3306");
+    define("CHARSET", "utf8");
+?>
+
